397b7a70d9926f8dff31f34842147739ed214459
Ram Bansal <ram.bansal@nylas.com>
2024-07-16 10:32:12 +0200

    update init repo

---
+++ util/calculateSimilarity.js
@@ -0,0 +1,8 @@
+function calculateSimilarity(vec1, vec2) {
+  const dotProduct = vec1.reduce((sum, val, i) => sum + val * vec2[i], 0);
+  const magnitude1 = Math.sqrt(vec1.reduce((sum, val) => sum + val * val, 0));
+  const magnitude2 = Math.sqrt(vec2.reduce((sum, val) => sum + val * val, 0));
+  return dotProduct / (magnitude1 * magnitude2);
+}
+
+export { calculateSimilarity }
